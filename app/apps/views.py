5a6c6277d6a866ea12ca26f2982778bd55f83e42
Younger <925781609@qq.com>
2017-01-30 14:35:18 +0800

    add redirect import

--- app/apps/views.py
+++ app/apps/views.py
@@ -1,6 +1,6 @@
 from . import apps
 from random import randint
-from flask import render_template, flash, url_for, session
+from flask import render_template, redirect, flash, url_for, session
 from flask_wtf import Form
 from wtforms import IntegerField, SubmitField
 from wtforms.validators import Required, NumberRange
@@ -41,5 +41,6 @@ def guess_number():
         else:
             flash(u'You win')
             start_to_guess = 1
+            return redirect(url_for(main.index))
     return render_template('guess.html', form=form)
 
