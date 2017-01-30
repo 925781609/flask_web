639295fab7b32018c34f8abc2a6b5984f913e4bf
Younger <925781609@qq.com>
2017-01-30 14:28:07 +0800

    only initialize the date to be guess and guessing times at 1st time

--- app/apps/views.py
+++ app/apps/views.py
@@ -4,7 +4,7 @@ from flask import render_template, flash, url_for, session
 from flask_wtf import Form
 from wtforms import IntegerField, SubmitField
 from wtforms.validators import Required, NumberRange
-
+start_to_guess  = 1
 class GuessNumberForm(Form):
     number = IntegerField(u'Please guess a number between 0 and 1000:', 
                           validators=[Required(u'Please input a valid number'), 
@@ -14,8 +14,11 @@ class GuessNumberForm(Form):
 @apps.route('/guess_number', methods=['GET', 'POST'])
 def guess_number():
     #Generate an integer between 0 and 1000, and save it in session
-    session['number'] = randint(0, 1000)
-    session['times'] = 10
+    global start_to_guess
+    if start_to_guess:
+        session['number'] = randint(0, 1000)
+        session['times'] = 10
+        start_to_guess = 0
     print('guess_number() was called')
     times = session['times']
     result = session.get('number') 
@@ -28,6 +31,7 @@ def guess_number():
         if times == 0:
             flash(u'Failed to guess the right number ....o(>_<)o')
             flash(u'The real number is {}'.format(result))
+            start_to_guess = 1
             return redirect(ur_for('main.index'))
         answer = form.number.data
         if answer > result:
@@ -36,5 +40,6 @@ def guess_number():
             flash(u'Too small! you still have %s times chance' % times)
         else:
             flash(u'You win')
+            start_to_guess = 1
     return render_template('guess.html', form=form)
 
