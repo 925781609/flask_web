2fc459ff7008352e676edca04fcc7e38776e6fa5
Younger <925781609@qq.com>
2017-01-30 12:43:15 +0800

    fix typo

--- app/apps/views.py
+++ app/apps/views.py
@@ -28,7 +28,7 @@ def guess_number():
             flash(u'The real number is {}'.format(result))
             return redirect(ur_for('main.index'))
         answer = form.number.data
-        if ansewer > result:
+        if answer > result:
             flash(u'Too big! you still have %s times chance' % times)
         elif answer < result:
             flash(u'Too small! you still have %s times chance' % times)
